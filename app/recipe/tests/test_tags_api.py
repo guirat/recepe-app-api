7cad29814db4dd3520e023dc8759aa905ca07eab
guirat <guirat.meds@gmail.com>
2021-07-09 15:54:31 +0200

    (feat) list ingredient

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from recipe.serializer import TagSerializer
+from recipe.serializers import TagSerializer
 
 from core.models import Tag
 
