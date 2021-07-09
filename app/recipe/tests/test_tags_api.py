fc5318666ad7d255c7beb75d37d31b560b1b9ad0
guirat <guirat.meds@gmail.com>
2021-07-09 15:14:27 +0200

    (feat) add tag create feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from recipe.serializers import TagSerializer
+from recipe.serializer import TagSerializer
 
 from core.models import Tag
 
@@ -33,8 +33,8 @@ class PrivateTagsApiTests(TestCase):
             'test@vispesious.com',
             'testpass'
         )
-        self.client = APIClient
-        self.client.force_authenticate(self.user)
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
 
     def test_retrieve_tags(self):
         """Test retrieving tags"""
@@ -46,11 +46,11 @@ class PrivateTagsApiTests(TestCase):
         tags = Tag.objects.all().order_by('-name')
         serializer = TagSerializer(tags, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.asssertEqual(res.data, serializer.data)
+        self.assertEqual(res.data, serializer.data)
 
     def test_tags_limited_to_user(self):
         """Test that tags returned are for the authentcated user"""
-        user2 = get_user_model().objects.create(
+        user2 = get_user_model().objects.create_user(
             'other@vispesious.com',
             'testpass'
         )
@@ -61,3 +61,21 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successful(self):
+        """Test creating a new tag"""
+        payload = {'name': 'Test tag'}
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
